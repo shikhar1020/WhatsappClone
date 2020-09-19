0b84686296b0488baa505b2d0b0562ffbe33947a
Shikhar <shikhar.17.srivastava@st.niituniversity.in>
2020-09-19 23:42:18 +0530

    WhatsApp Initial Setup & Firebase Configuration

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAQnv-f1hPmlrGxCjFwe8gC77PBZVEwWlE",
+    authDomain: "whatsappclonemern-7b264.firebaseapp.com",
+    databaseURL: "https://whatsappclonemern-7b264.firebaseio.com",
+    projectId: "whatsappclonemern-7b264",
+    storageBucket: "whatsappclonemern-7b264.appspot.com",
+    messagingSenderId: "769899882330",
+    appId: "1:769899882330:web:3535f33bd48d4d9fd87563",
+    measurementId: "G-XMYRHE4JFK"
+  };
