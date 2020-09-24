76e99d0a38ba703ccd15623e343fef475db36246
Shikhar <shikhar.17.srivastava@st.niituniversity.in>
2020-09-24 09:33:05 +0530

    Fontend Major

--- src/chats.js
+++ src/chats.js
@@ -18,6 +18,10 @@ function Chats() {
         <div className="chats">
             <div className="chats_header">
             <Avatar alt="Shikhar Sangam" src="https://avatars1.githubusercontent.com/u/54438024?s=460&u=6312f0e7142c4ed394a8fb9a4254cba4325c9fe7&v=4" />
+                <div className="chatHeader_info">
+                    <h3>Sangam Doobara</h3>
+                    <p>last seen at 19:00</p>
+                </div>
                 <div className="chats_headerRight">
                     <IconButton>
                         <SearchIcon/>
@@ -32,23 +36,100 @@ function Chats() {
             </div>
 
             <div className="mainChat">
-                <h3>Main Chat Content!</h3>
+                <p className="mainChat_sentmessage">
+                    <span className="chat_name">Sangam</span>
+                    Tujhe yaad karke, mujhe chain aawe...Tujhe yaad karke, mujhe chain aawe...Tujhe yaad karke, mujhe chain aawe...
+                    <span className="chat_timestamp">{new Date().toLocaleTimeString()}</span>            
+                </p>
+
+                <p className="mainChat_sentmessage mainChat_receivedmessage">
+                    <span className="chat_name">Sangam</span>
+                    Tujhe yaad karke, mujhe chain aawe...Tujhe yaad karke, mujhe chain aawe...Tujhe yaad karke, mujhe chain aawe...Tujhe yaad karke, mujhe chain aawe...
+                    <span className="chat_timestamp">{new Date().toLocaleTimeString()}</span>            
+                </p>
+
+                <p className="mainChat_sentmessage">
+                    <span className="chat_name">Sangam</span>
+                    Tujhe yaad karke, mujhe chain aawe...
+                    <span className="chat_timestamp">{new Date().toLocaleTimeString()}</span>            
+                </p>
+
+                <p className="mainChat_sentmessage mainChat_receivedmessage">
+                    <span className="chat_name">Sangam</span>
+                    Tujhe yaad karke, mujhe chain aawe...
+                    <span className="chat_timestamp">{new Date().toLocaleTimeString()}</span>            
+                </p>
+
+                <p className="mainChat_sentmessage ">
+                    <span className="chat_name">Sangam</span>
+                    Tujhe yaad karke, mujhe chain aawe...
+                    <span className="chat_timestamp">{new Date().toLocaleTimeString()}</span>            
+                </p>
+
+                <p className="mainChat_sentmessage mainChat_receivedmessage">
+                    <span className="chat_name">Sangam</span>
+                    Tujhe yaad karke, mujhe chain aawe...
+                    <span className="chat_timestamp">{new Date().toLocaleTimeString()}</span>            
+                </p>
+                
+                <p className="mainChat_sentmessage">
+                    <span className="chat_name">Sangam</span>
+                    Tujhe yaad karke, mujhe chain aawe...
+                    <span className="chat_timestamp">{new Date().toLocaleTimeString()}</span>            
+                </p>
+
+                <p className="mainChat_sentmessage">
+                    <span className="chat_name">Sangam</span>
+                    Tujhe yaad karke, mujhe chain aawe...
+                    <span className="chat_timestamp">{new Date().toLocaleTimeString()}</span>            
+                </p>
+
+                <p className="mainChat_sentmessage">
+                    <span className="chat_name">Sangam</span>
+                    Tujhe yaad karke, mujhe chain aawe...
+                    <span className="chat_timestamp">{new Date().toLocaleTimeString()}</span>            
+                </p>
+
+                <p className="mainChat_sentmessage">
+                    <span className="chat_name">Sangam</span>
+                    Tujhe yaad karke, mujhe chain aawe...
+                    <span className="chat_timestamp">{new Date().toLocaleTimeString()}</span>            
+                </p>
+
+                <p className="mainChat_sentmessage">
+                    <span className="chat_name">Sangam</span>
+                    Tujhe yaad karke, mujhe chain aawe...
+                    <span className="chat_timestamp">{new Date().toLocaleTimeString()}</span>            
+                </p>
+
+                <p className="mainChat_sentmessage">
+                    <span className="chat_name">Sangam</span>
+                    Tujhe yaad karke, mujhe chain aawe...
+                    <span className="chat_timestamp">{new Date().toLocaleTimeString()}</span>            
+                </p>
+
             </div>
 
-            <div className="chatSend">
+            <form>
+            <div className="chatSend">            
                 <IconButton>
                     <SentimentVerySatisfiedIcon/>
-                </IconButton>
-                <div className="chatSend_messageContainer">
-                    <input placeholder="Type a message" type="text" size="73"/>
-                </div>
-                <IconButton>
-                    <SendIcon/>
-                </IconButton>
-                <IconButton>
-                    <MicIcon/>
-                </IconButton>                
-            </div>
+                </IconButton>                                
+                    <div className="chatSend_messageContainer">
+                        <input placeholder="Type a message" type="text" size="70"/>
+                        {/*<button type="submit">Send</button>*/}
+                        <IconButton>
+                            <AttachFileIcon/>
+                        </IconButton>      
+                    </div>
+                    <IconButton>
+                        <SendIcon type="submit"/>
+                    </IconButton>
+                    <IconButton>
+                        <MicIcon/>
+                    </IconButton>                                                                                              
+            </div> 
+            </form>       
         </div>
     )
 }
