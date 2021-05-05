5635d9ef07fb60881a7cef2ae34deea65a0a5e64
Shikhar Srivastava <54438024+shikhar1020@users.noreply.github.com>
2021-05-05 17:59:22 +0530

    updated backend url

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:9000'
+    baseURL: 'https://mernwhatsapp-backend.herokuapp.com/'
+    // 'http://localhost:9000'
 });
 
 export default instance;
