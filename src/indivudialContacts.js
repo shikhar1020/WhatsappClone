0d2fdf36d386f7b9094000276254c457401a2cfd
Shikhar Srivastava <54438024+shikhar1020@users.noreply.github.com>
2021-05-05 10:27:18 +0530

    restart

--- src/indivudialContacts.js
+++ src/indivudialContacts.js
@@ -9,7 +9,7 @@ function IndivudialContacts() {
         <Avatar alt="Shikhar Sangam" src="https://avatars1.githubusercontent.com/u/54438024?s=460&u=6312f0e7142c4ed394a8fb9a4254cba4325c9fe7&v=4" />
         <div className="IndivudialContacts_info">
             <h4>Sangam Pyaru</h4>
-            <p>Tabhi toh itna maza aaya tha</p>
+            <p>Tabhi toh itna maza aaya tha!</p>
         </div>
     </div>
 
